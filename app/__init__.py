2484874cb1d184501ec01aa48e3be9ea39af602b
galind <guillem.yt1@gmail.com>
2023-04-03 01:36:58 +0200

    Added db init

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
 from config import Config
+from app.extensions import db
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    db.init_app(app)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
