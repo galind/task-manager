94a3382da646731b15c73b76cb7e0e1e0038ecf7
galind <guillem.yt1@gmail.com>
2023-04-03 20:06:38 +0200

    Added status and price

--- app/models/task.py
+++ app/models/task.py
@@ -5,6 +5,8 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     description = db.Column(db.Text)
+    status = db.Column(db.String(20))
+    price = db.Column(db.Integer)
 
     def __repr__(self) -> str:
         return f'Task "{self.title}"'
