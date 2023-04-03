3e847edcef29f530a7bb73a8f5f1af036c5a9aa3
galind <guillem.yt1@gmail.com>
2023-04-03 18:26:17 +0200

    File created

---
+++ app/models/task.py
@@ -0,0 +1,10 @@
+from app.extensions import db
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    description = db.Column(db.Text)
+
+    def __repr__(self) -> str:
+        return f'Task "{self.title}"'
