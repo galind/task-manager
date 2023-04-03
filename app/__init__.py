3c2d048568d4cd852834588bb0dd363c0a70d98e
galind <guillem.yt1@gmail.com>
2023-04-04 00:54:33 +0200

    Added db create all

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,10 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    db.init_app(app)
+    with app.app_context():
+        db.init_app(app)
+
+        db.create_all()
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
