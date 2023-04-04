f485e54266b7e8c9857d4e511baf04228d84dce0
galind <guillem.yt1@gmail.com>
2023-04-04 12:39:58 +0200

    Updated tasks query all

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from app.main import bp
+from app.models.task import Task
 
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    tasks = [t.__dict__ for t in Task.query.all()]
+    return render_template('index.html', tasks=tasks)
