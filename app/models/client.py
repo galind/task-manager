4dd625137c6624e2e7b64ee78a46d1eb37708f78
galind <guillem.yt1@gmail.com>
2023-04-04 12:40:13 +0200

    File removed

--- app/models/client.py
+++
@@ -1,9 +0,0 @@
-from app.extensions import db
-
-
-class Client(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-
-    def __repr__(self) -> str:
-        return f'Client "{self.name}"'
